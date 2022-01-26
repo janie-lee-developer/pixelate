b5929f576ddcad33ba2459e8f5e022702419d708
Robert Albenda <ralbenda@gmail.com>
2022-01-25 20:54:35 -0500

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-// Your code here
+
+const table = document.querySelector('table');
+console.log(table)
+
+
+function makeRow () {
+    const tr = document.createElement('tr');
+    for(let i =0; i < 20; i++) {
+        const td = document.createElement('td');
+        tr.appendChild(td);
+        
+    }
+   
+    
+    table.appendChild(tr);
+}
+
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', makeRow);
+
+let color = 'red';
+
+const colorize = (event) => {
+    
+    if(!event.target.className) {
+        event.target.classList.add(color);
+    }
+    else {
+        event.target.className = '';
+    }
+    console.log('clicked',event.target.value);
+}
+
+const toggleColors = (event) => {
+    color = event.target.value;
+
+
+}
+
+table.addEventListener('click', colorize)
+
+const select = document.querySelector('select');
+
+
+select.addEventListener('change', toggleColors )
